d948ca0dd36e89771a565b7648088ec204695ccf
erosrolf <erosrolf@gmail.com>
2024-08-18 21:00:48 +0300

    добавил портал и спавн орков, сделал фабрики синглтонами.

--- Assets/BrokenLoop/Scripts/Gameplay/Enemies/MeleeOrkEnemy.cs
+++ Assets/BrokenLoop/Scripts/Gameplay/Enemies/MeleeOrkEnemy.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using MovementSystem;
 using MovementSystem.Interfaces;
 using UnityEngine;
@@ -6,25 +7,34 @@ using UnityEngine;
 namespace BrokenLoop.Gameplay
 {
     [RequireComponent(typeof(MovementWithRigidbody2d))]
+    [RequireComponent(typeof(SpriteRenderer))]
     public class MeleeOrkEnemy : BaseEnemy, IDamagable, IAttackable, IMovement
     {
         private MovementWithRigidbody2d _movement;
-        private Vector2[] _movementPath;
+        private Queue<Vector2Int> _movementPath;
+        private bool _constructed;
+        private SpriteRenderer _spriteRenderer;
 
         private void FixedUpdate()
         {
-            throw new NotImplementedException();
+            if (_constructed == false)
+                return;
+            
+            Move((Vector3)CalculateDirection());
         }
-
-        public void Construct(int health, Vector2[] movementPath)
+        
+        public override void Construct(string id, int health, int damage, Vector2Int[] path)
         {
             _health = new Health(health, gameObject);
-            _movementPath = movementPath;
+            _movementPath = new Queue<Vector2Int>(path);
             _movement = GetComponent<MovementWithRigidbody2d>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _constructed = true;
         }
 
         public override void Move(Vector3 direction)
         {
+            _spriteRenderer.flipX = direction.x > 0;
             _movement.Move(direction);
         }
 
@@ -32,5 +42,31 @@ namespace BrokenLoop.Gameplay
         {
             //TODO: ork attack
         }
+
+        private Vector2 CalculateDirection()
+        {
+            if (_movementPath.Count == 0)
+            {
+                return Vector2.zero;
+            }
+            
+            Vector2Int tilePosition = new Vector2Int(
+                Mathf.FloorToInt(transform.position.x),
+                Mathf.FloorToInt(transform.position.y)
+            );
+            if (_movementPath.First() == tilePosition)
+            {
+                _movementPath.Dequeue();
+            }
+            
+            if (_movementPath.Count == 0)
+            {
+                return Vector2.zero;
+            }
+
+            Vector2 movementVector = _movementPath.First() - tilePosition;
+            return movementVector.normalized;
+        }
+        
     }
 }
