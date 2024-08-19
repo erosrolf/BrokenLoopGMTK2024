b790427918c92facd23e0a3a021a1571fd3ac95f
erosrolf <erosrolf@gmail.com>
2024-08-19 16:10:08 +0300

    переименовал Towers в Buildings директорию внутри Resources

---
+++ Assets/BrokenLoop/Scripts/Gameplay/CombatSystem/Attack/BoomInTile.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace BrokenLoop.Gameplay
+{
+    [RequireComponent(typeof(Animator))]
+    public class BoomInTile : MonoBehaviour, IAttackable
+    {
+        private Animator _animator;
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+        }
+
+        public void Attack(IDamagable target, IAttackStrategy attackStrategy)
+        {
+            _animator.Play("Boom");
+            throw new System.NotImplementedException();
+        }
+    }
+}
