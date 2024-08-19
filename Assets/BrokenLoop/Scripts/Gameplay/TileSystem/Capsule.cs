dbd7319519571edd559be9ae9e6330bd8cefefaa
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-08-19 12:43:04 +0300

    add: anim ping

--- Assets/BrokenLoop/Scripts/Gameplay/TileSystem/Capsule.cs
+++ Assets/BrokenLoop/Scripts/Gameplay/TileSystem/Capsule.cs
@@ -1,16 +1,21 @@
 ﻿using BrokenLoop.Gameplay;
 using System.Collections;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 namespace Assets.BrokenLoop.Scripts.Gameplay.TileSystem
 {
     public class Capsule: MonoBehaviour
     {
         public int timeSpawnSecond = 5;
+        public float timeSpeedPing = 5;
+        public float sizeAlpha = 0.1f;
+        public float minAlpha = 0.5f;
+        public SpriteRenderer[] spriteRenderer;
         private void Start()
         {
+            StartCoroutine(Ping());
             StartCoroutine(Timer());
-            ;
         }
         IEnumerator Timer()
         {
@@ -19,6 +24,21 @@ namespace Assets.BrokenLoop.Scripts.Gameplay.TileSystem
             int tower = Random.Range(0, 2);
             BuildingsFactory.Instance.CreateInstance((EBuildingType)tower,gameObject.transform.position);
             Destroy(gameObject);
+        }
+        IEnumerator Ping()
+        {
+            while(gameObject.active)
+            {
+                yield return new WaitForSeconds(timeSpeedPing);
+                foreach (var item in spriteRenderer)
+                {
+                    item.color = new Color(1, 1, 1, item.color.a - sizeAlpha);
+                }
+                if (spriteRenderer[3].color.a < minAlpha || spriteRenderer[3].color.a >= 1)
+                {
+                    sizeAlpha *= -1;
+                }
+            }
         }
     }
 }
