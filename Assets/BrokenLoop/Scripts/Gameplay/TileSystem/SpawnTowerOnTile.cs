b8bcad7da0ef9c5bc71d672fb71c9fef16cd8222
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-08-19 16:20:39 +0300

    fix: исправил спавн вргов

--- Assets/BrokenLoop/Scripts/Gameplay/TileSystem/SpawnTowerOnTile.cs
+++ Assets/BrokenLoop/Scripts/Gameplay/TileSystem/SpawnTowerOnTile.cs
@@ -1,32 +1,66 @@
-﻿using Assets.BrokenLoop.Scripts.Gameplay.TileSystem;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class SpawnTowerOnTile: MonoBehaviour
+namespace Assets.BrokenLoop.Scripts.Gameplay.TileSystem
 {
-    public Tilemap tilemap;
-    public Camera camera;
-    PositionOnTileCamera setObjectOnTile;
-    public GameObject capsulePredab;
-    public int SpawnSecond = 2;
-    private void Start()
+    public class SpawnTowerOnTile : MonoBehaviour
     {
-        setObjectOnTile = new(tilemap, camera);
-        StartCoroutine(Spawner());
-    }
-     
-    IEnumerator Spawner()
-    {
-        while(gameObject.activeSelf)
+        
+        public GameObject capsule;
+        public int timeSpawnCapsule = 10;
+        private void Start()
         {
-            yield return new WaitForSeconds(SpawnSecond);
-            CreateCapsule();
+            StartCoroutine(Spawner());
         }
-    }
-    private void CreateCapsule() 
-    {
-        var objectSpawn = Instantiate(capsulePredab);
-        setObjectOnTile.SetObjectOnMapRandom(objectSpawn);
+
+
+        IEnumerator Spawner()
+        {
+            TileResourceForMove tileResource = TileResourceForMove.Instance;
+            while (gameObject.activeSelf)
+            {
+                yield return new WaitForSeconds(timeSpawnCapsule);
+                int i = Random.Range(0, tileResource.GetListMoveTile().Count);
+                Instantiate(capsule, tileResource.GetListMoveTile()[i],Quaternion.identity);
+            }
+        }
+        //private void Update()
+        //{
+        //    if (Input.GetKeyDown(KeyCode.W))
+        //    {
+        //        Push(new Vector3Int(0,1,0));
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.S))
+        //    {
+        //        Push(new Vector3Int(0, -1, 0));
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.D))
+        //    {
+        //        Push(new Vector3Int(1, 0, 0));
+        //    }
+        //    else if (Input.GetKeyDown(KeyCode.A))
+        //    {
+        //        Push(new Vector3Int(-1, 0, 0));
+        //    }
+        //    if(Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        RefreshGround();
+        //    }
+        //}
+       //public bool Push(Vector3Int direction)
+       //{
+       //    var newPosition = _gameObject.transform.position + direction;
+       //    var canMove = filledCellWorldPositions.Contains(newPosition);
+       //    if (canMove)
+       //    {
+       //        _gameObject.transform.position = newPosition;
+       //        return true;
+       //    }
+       //    return false;
+       //}
     }
 }
