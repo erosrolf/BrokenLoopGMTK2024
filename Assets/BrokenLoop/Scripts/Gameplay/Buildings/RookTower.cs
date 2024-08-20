8eb04de26ada194f57c6b76ebb13ad3d7ed004fc
erosrolf <erosrolf@gmail.com>
2024-08-20 13:47:42 +0300

    добавил префаб взрыва, сделал нанесение урона через него (это я зря=))

--- Assets/BrokenLoop/Scripts/Gameplay/Buildings/RookTower.cs
+++ Assets/BrokenLoop/Scripts/Gameplay/Buildings/RookTower.cs
@@ -1,24 +1,54 @@
-using System;
+using System.Collections;
 using BrokenLoop.Gameplay;
 using MovementSystem.Interfaces;
 using UnityEngine;
 
 namespace BrokenLoop.Scripts.TileObjects
 {
+    [RequireComponent(typeof(BoomPlusAttack))]
     public class RookTower : BaseBuilding, IAttackable, IDamagable, IMovement
     {
-        private IAttackStrategy _attackStrategy;
+        private BoomPlusAttack _boomPlusAttack;
+
+        [SerializeField, Range(0.1f, 1f)] private float _cooldown;
+        public bool IsCanAttack { get; private set; }
 
         private void Awake()
         {
             Health = 10;
             ID = _lastID++.ToString();
-            _attackStrategy = new AttackInCellStrategy(transform.position, 1);
+            _boomPlusAttack = GetComponent<BoomPlusAttack>();
+            IsCanAttack = true;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(AttackRoutine(3f));
         }
 
         public void Attack()
         {
-            throw new System.NotImplementedException();
+            if (IsCanAttack)
+            {
+                _boomPlusAttack.Attack();
+                IsCanAttack = false;
+                StartCoroutine(ResetAttackCooldown());
+            }
+        }
+
+        private IEnumerator ResetAttackCooldown()
+        {
+            yield return new WaitForSeconds(_cooldown);
+            IsCanAttack = true;
+        }
+
+        private IEnumerator AttackRoutine(float delay)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(delay);
+                _boomPlusAttack.Attack();
+            }
         }
 
         public void Move(Vector3 direction)
--- Assets/BrokenLoop/Scripts/Gameplay/Buildings/RoundTower.cs
+++ Assets/BrokenLoop/Scripts/Gameplay/Buildings/RoundTower.cs
@@ -1,24 +1,35 @@
-using BrokenLoop.Gameplay;
+using System.Collections;
 using MovementSystem.Interfaces;
 using UnityEngine;
 
 namespace BrokenLoop.Gameplay
 {
+    [RequireComponent(typeof(BoomXAttack))]
     public class RoundTower : BaseBuilding, IAttackable, IDamagable, IMovement
     {
-        private IAttackStrategy _attackStrategy;
+        [SerializeField, Range(0.1f, 1f)] private float _cooldown;
+        private BoomXAttack _boomXAttack;
+        
+        public bool IsCanAttack { get; private set; }
         
         private void Awake()
         {
             Health = 10;
             ID = _lastID++.ToString();
+            _boomXAttack = GetComponent<BoomXAttack>();
+            IsCanAttack = true;
+        }
+        
+        private void Start()
+        {
+            StartCoroutine(AttackRoutine(3f));
         }
 
         public void Attack()
         {
-            throw new System.NotImplementedException();
+            StartCoroutine(AttackRoutine(3f));
         }
-
+        
         public void Move(Vector3 direction)
         {
             throw new System.NotImplementedException();
@@ -27,6 +38,21 @@ namespace BrokenLoop.Gameplay
         public void RestorePosition(Vector3 position)
         {
             transform.position = position;
+        }
+        
+        private IEnumerator ResetAttackCooldown()
+        {
+            yield return new WaitForSeconds(_cooldown);
+            IsCanAttack = true;
+        }
+
+        private IEnumerator AttackRoutine(float delay)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(delay);
+                _boomXAttack.Attack();
+            }
         }
     }
 }
